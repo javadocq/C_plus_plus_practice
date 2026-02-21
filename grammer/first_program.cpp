7e4d59264b6baaebbf3bbf59b8249891aa51a741
javadocq <han18264@naver.com>
2026-02-21 17:13:50 +0900

    first_program

---
+++ grammer/first_program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int favorite_number;
+
+    cout << "Enter your favorite number between 1 and 100: ",
+    cin >> favorite_number;
+    cout << "Amazing!! That's my favorite number too! " << favorite_number << endl;
+
+    return 0;
+}
