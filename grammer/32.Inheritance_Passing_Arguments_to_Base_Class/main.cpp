5e56e12d4455c03cf1824fb68c26860f4a010b21
javadocq <han18264@naver.com>
2026-02-26 22:30:08 +0900

    31.Inheritance_Passing_Arguments_to_Base_Class

--- grammer/32.Inheritance_Passing_Arguments_to_Base_Class/main.cpp
+++ grammer/32.Inheritance_Passing_Arguments_to_Base_Class/main.cpp
@@ -23,7 +23,7 @@ public:
 	Derived() : double_value{ 0 } { // Default constructor도 변수는 초기화 해주면 좋다
 		cout << "Derived Constructor" << endl;
 	}
-	Derived(int val) : Base{ val }, double_value{ val * 2 } { // 만약, val 값이 들어올 때, Base 클래스의 생성자도 호출이 되는데 아무것도 명시를 하지 않으면 기본 생성자가 호출된다. 그래서 명시적으로 Base 클래스의 생성자를 호출해주는 것이 좋다.
+	Derived(int val) : Base{val}, double_value { val * 2 } { // 만약, val 값이 들어올 때, Base 클래스의 생성자도 호출이 되는데 아무것도 명시를 하지 않으면 기본 생성자가 호출된다. 그래서 명시적으로 Base 클래스의 생성자를 호출해주는 것이 좋다.
 		cout << "Derived Constructor with value: " << double_value << endl;
 	}
 	~Derived() {
