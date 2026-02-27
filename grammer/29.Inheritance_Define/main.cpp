0c8b61811c912a0b6c1875836a2186a42f31cdd8
javadocq <han18264@naver.com>
2026-02-27 18:57:38 +0900

    41.Operator_Overloading_Introduction

--- grammer/29.Inheritance_Define/main.cpp
+++ grammer/29.Inheritance_Define/main.cpp
@@ -2,39 +2,70 @@
 
 using namespace std;
 
-class IEntity { // 순수 가상 함수만을 가졌기 때문에 인터페이스 클래스이다. 통상  IEntity와 같이 I로 시작하는 이름을 가진다.
-protected:
-	int x, y;
+class Number {
+private:
+	int value;
 public:
-	IEntity(int x, int y) : x(x), y(y) {}
-	virtual ~IEntity() {
-		cout << "Entity destructor called" << endl;
+	Number(int value) : value(value) {}
+
+	// 연산자 오버로딩
+	Number Add(Number other) {
+		return Number{ value + other.value };
 	}
-	virtual void Move(int dx, int dy) = 0;
-	virtual void PrintPosition() const = 0;
+	int GetValue() const {
+		return value;
+	}
+	void display() {
+		cout << "Value: " << value << endl;
+	}
 };
 
-class Player : public IEntity {
+Number Add(Number n1, Number n2) {
+	return Number{ n1.GetValue() + n2.GetValue()};
+
+}
+
+class Player {
+
+};
+
+class Point {
 private:
-	int hp;
-	int xp;
+	int x;
+	int y;
 public:
-	Player(int x, int y, int hp, int xp) : IEntity(x, y), hp{ hp }, xp{ xp } {}
-	virtual ~Player() {
-		cout << "Player destructor called" << endl;
+	Point(int x = 0, int y = 0) : x(x), y(y) {}
+	// 연산자 오버로딩
+	Point operator+(const Point& other) {
+		return Point{ x + other.x, y + other.y };
 	}
-	virtual void Move(int dx, int dy) { 
-		x += dx * 2;
-		y += dy * 2;
-	}
-	virtual void PrintPosition() const override {
-		cout << "Player position: " << x << ", " << y << endl;
+	void display() {
+		cout << "Point(" << x << ", " << y << ")" << endl;
 	}
 };
 
 int main() {
-	// Entity e{ 0, 0 }; // Entity 클래스는 추상 클래스이므로 객체를 생성할 수 없다. 컴파일 에러가 발생한다.
-	Entity* ptr = new Player(0, 0, 100, 0); // 하지만 여전히 포인터/참조자를 사용해 오버라이딩된 함수를 동적 바인딩은 할 수 있다.
-	Player p{ 0, 0, 100, 0 }; // Player class에서 만약 Move 함수를 구현하지 않았다면, Player 클래스도 추상 클래스가 되어 객체를 생성할 수 없다. 컴파일 에러가 발생한다.
+	Player player1;
+	Player player2;
+
+	// Player p3 = p1 + p2; 불가능 
+	// int 타입 이런 거는 사전에 컴파일러가 알 수 있지만, 사용자 정의는 몇 바이트를 할당해야하는지 모르기 때문에 불가능하다.
+
+	int a = 1;
+	int b = 2;
+	int c = a + b; // 가능
+
+	Number n1{ 10 };
+	Number n2{ 20 };
+
+	Number n3 = n1.Add(n2); // 첫번째 방법
+	n3.display();
+	Number n4 = Add(n1, n2); // 두번째 방법
+	n4.display();
+
+	Point p1{ 1, 2 };
+	Point p2{ 3, 4 };
+	Point p3 = p1 + p2; // 연산자 오버로딩을 통해 가능
+
 	return 0;
 }
